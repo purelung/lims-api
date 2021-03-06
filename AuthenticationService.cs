fc7fe04da8f61d0aaec76804facd9446103a6a1b
Sean Wolf <swolf@NABDEN-LMBP2.local>
2021-03-05 21:36:49 -0700

    added jwt authentication service

---
+++ AuthenticationService.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace AzFunctionsJwtAuth
+{
+    /// <summary>
+    ///     Service class for performing authentication.
+    /// </summary>
+    public class AuthenticationService
+    {
+        private readonly TokenIssuer _tokenIssuer;
+
+        /// <summary>
+        ///     Injection constructor.
+        /// </summary>
+        /// <param name="tokenIssuer">DI injected token issuer singleton.</param>
+        public AuthenticationService(TokenIssuer tokenIssuer)
+        {
+            _tokenIssuer = tokenIssuer;
+        }
+
+        [FunctionName("Authenticate")]
+        public async Task<IActionResult> Authenticate(
+            // https://stackoverflow.com/a/52748884/116051
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "auth")] HttpRequest req,
+            ILogger log)
+        {
+            // Perform custom authentication here
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var tokenResult = await GoogleTokenHelper.TokenIsValid(requestBody);
+
+            if (!tokenResult.IsValid)
+            {
+                return new UnauthorizedResult();
+            }
+
+            var creds = new Credentials() { User = tokenResult.Email, Password = "" };
+
+            var jwt = new { token = _tokenIssuer.IssueTokenForUser(creds) };
+
+            return new JsonResult(jwt);
+        }
+    }
+
+
+    public struct GoogleTokenResult
+    {
+        public GoogleTokenResult(bool isValid, string email = null)
+        {
+            IsValid = isValid;
+            Email = email;
+        }
+
+        public bool IsValid;
+        public string? Email;
+    }
+
+    public class GoogleTokenHelper
+    {
+        public static async Task<GoogleTokenResult> TokenIsValid(string token)
+        {
+
+            string baseUrl = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=";
+
+            try
+            {
+
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(baseUrl + token))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+
+                            if (data != null)
+                            {
+
+                                Console.WriteLine("data------------{0}", data);
+
+
+                                if (data.Contains("Invalid Value"))
+                                {
+                                    return new GoogleTokenResult(false);
+                                }
+
+                                return new GoogleTokenResult(true, "get_email_from_data");
+                            }
+                            else
+                            {
+                                Console.WriteLine("NO Data----------");
+                                return new GoogleTokenResult(false);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception Hit------------");
+                Console.WriteLine(exception);
+                return new GoogleTokenResult(false);
+            }
+
+        }
+    }
+
+    public class Credentials
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
--- getCustomers.cs
+++ getCustomers.cs
@@ -6,15 +6,16 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Net.Http;
+using AzFunctionsJwtAuth;
 
 namespace ZeeReportingApi
 {
-    public static class getCustomers
+    public class getCustomers : AuthorizedServiceBase
     {
         [FunctionName("getCustomers")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -22,12 +23,11 @@ namespace ZeeReportingApi
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            log.LogInformation(requestBody);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            string responseMessage = "booya gpa";
 
             return new OkObjectResult(responseMessage);
         }
